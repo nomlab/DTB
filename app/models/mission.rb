67b3f5b9ec15eb94a5b7df1ec22fd13eed830efb
Takuya Okada <pitipitiunsyumikan@gmail.com>
2013-12-10 13:03:00 +0900

    Define relationships of models

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,2 +1,4 @@
 class Mission < ActiveRecord::Base
+  has_many :children, :class_name => "Mission", :foreign_key => :parent_id
+  belongs_to :parent, :class_name => "Mission", :foreign_key => :parent_id
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  has_many :time_entries
+  belongs_to :mission
 end
