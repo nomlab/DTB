d9a75c9be61f7b404dd21eb3f5fa819edbb099f7
Takuya Okada <pitipitiunsyumikan@gmail.com>
2014-11-28 00:05:57 +0900

    Add two path to assets path

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,7 @@ module DTB
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/lib)
     config.time_zone = 'Tokyo'
+    config.assets.paths << Rails.root.join("vendor", "assets", "jquery-treegrid", "js").to_s
+    config.assets.paths << Rails.root.join("vendor", "assets", "jquery-treegrid", "css").to_s
   end
 end
