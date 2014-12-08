671a7433812b9e00840d4e461cb5cb3b41e330fc
Takuya Okada <pitipitiunsyumikan@gmail.com>
2014-12-08 13:50:30 +0900

    Change script for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+unless State.find_by(name: "TODO")
+  default = State.find_by(default: true).nil?
+  State.create( name: "TODO",
+                color: "#e65757",
+                position: 1,
+                default: default )
+end
+unless State.find_by(name: "DONE")
+  State.create( name: "DONE",
+                color: "#00ff40",
+                position: 2,
+                default: false )
+end
--- local/demo/init_demo.rb
+++ local/demo/init_demo.rb
@@ -2,22 +2,34 @@
 DEMO_DIR = Rails.root.join('local').join('demo')
 WEEK = 7
 
+todo = State.create( name: "TODO",
+                     color: "#e65757",
+                     position: 1,
+                     default: true )
+done = State.create( name: "DONE",
+                     color: "#00ff40",
+                     position: 2,
+                     default: false )
+
 m_p2 = Mission.create( name:        "プログラミング演習TA",
                        description: "プログラミング演習TAに関する業務",
                        deadline:    DateTime.new(2014, 7, 28, 0, 0, 0, '+9'),
-                       keyword:     "p2 TA" )
+                       keyword:     "p2 TA",
+                       parent_id:   0, # root mission
+                       state_id:    todo.id)
 
 m_record = Mission.create( name:        "成績採点補助",
                            description: "プログラミング演習TA中の成績採点補助に関する業務",
                            deadline:    DateTime.new(2014, 7, 25, 0, 0, 0, '+9'),
                            keyword:     "p2 TA 成績",
-                           parent_id:   m_p2.id )
+                           parent_id:   m_p2.id,
+                           state_id:    todo.id)
 t_table = Task.create( name:        "出欠確認表作成",
                        description: "出欠確認表作成業務",
                        deadline:    DateTime.new(2014, 7, 20, 0, 0, 0, '+9'),
-                       status:      true,
                        keyword:     "成績",
-                       mission_id:  m_record.id )
+                       mission_id:  m_record.id,
+                       state_id:    todo.id )
 TimeEntry.create( name:       "出欠確認表作成",
                   keyword:    "確認表 xlsx",
                   comment:    "もれが発生しないよう他のTAと協調作業すること",
@@ -40,9 +52,9 @@ TimeEntry.create( name:       "出欠確認表作成",
 t_report = Task.create( name:        "レポート提出チェック",
                        description: "レポート提出チェック業務",
                        deadline:    DateTime.new(2014, 7, 25, 0, 0, 0, '+9'),
-                       status:      true,
                        keyword:     "レポート",
-                       mission_id:  m_record.id )
+                       mission_id:  m_record.id,
+                        state_id:    todo.id )
 TimeEntry.create( name:       "中間レポートチェック",
                   keyword:    "xlsx pdf",
                   comment:    "もれが発生しないよう他のTAと協調作業すること",
@@ -61,14 +73,16 @@ TimeEntry.create( name:       "期末レポートチェック",
                            description: "プログラミング演習TAの第#{n}回講義に関する業務",
                            deadline:    DateTime.new(2014, 4, 17, 0, 0, 0, '+9') + (WEEK * n-1),
                            keyword:     "p2 TA 講義",
-                           parent_id:   m_p2.id )
+                           parent_id:   m_p2.id,
+                           state_id:    done.id )
 
   t_prepare_lect = Task.create( name:        "講義準備",
                                 description: "講義準備業務",
                                 deadline:    DateTime.new(2014, 4, 17, 8, 40, 0, '+9') + (WEEK * n-1),
-                                status:      true,
                                 keyword:     "準備 講義",
-                                mission_id:  m_lect.id )
+                                mission_id:  m_lect.id,
+                                state_id:    done.id )
+
   TimeEntry.create( name:       "講義準備",
                     keyword:    "講義",
                     comment:    "プロジェクタをつけ忘れないこと",
@@ -79,23 +93,23 @@ TimeEntry.create( name:       "期末レポートチェック",
   t_shuttle = Task.create( name:        "シャトルシート配布",
                            description: "シャトルシート配布業務",
                            deadline:    DateTime.new(2014, 4, 17, 9, 0, 0, '+9') + (WEEK * n-1),
-                           status:      true,
                            keyword:     "シャトルシート",
-                           mission_id:  m_lect.id )
+                           mission_id:  m_lect.id,
+                           state_id:    done.id )
   TimeEntry.create( name:       "シャトルシート配布",
                     keyword:    "シャトルシート",
                     comment:    "2回配る事もあるので気をつける",
                     start_time: DateTime.new(2014, 4, 17, 8, 50, 0, '+9') + (WEEK * n-1),
                     end_time: DateTime.new(2014, 4, 17, 8, 55, 0, '+9') + (WEEK * n-1),
-                    task_id: t_shuttle.id)
+                    task_id: t_shuttle.id,)
 
 
   t_check = Task.create( name:        "講義資料確認",
                          description: "講義資料確認業務",
                          deadline:    DateTime.new(2014, 4, 17, 11, 55, 0, '+9') + (WEEK * n-1),
-                         status:      true,
                          keyword:     "講義資料",
-                         mission_id:  m_lect.id )
+                         mission_id:  m_lect.id,
+                         state_id:    done.id )
   TimeEntry.create( name:       "講義資料確認",
                     keyword:    "講義資料",
                     comment:    "誤字脱字をチェックする",
@@ -106,9 +120,9 @@ TimeEntry.create( name:       "期末レポートチェック",
   t_assist_teach = Task.create( name:        "学生への指導補助",
                                 description: "学生への指導補助業務",
                                 deadline:    DateTime.new(2014, 4, 17, 11, 55, 0, '+9') + (WEEK * n-1),
-                                status:      true,
                                 keyword:     "質問 回答",
-                                mission_id:  m_lect.id )
+                                mission_id:  m_lect.id,
+                                state_id:    done.id )
   TimeEntry.create( name:       "学生への指導補助",
                     keyword:    "指導 質問",
                     comment:    "学生からの質問内容はメモしておく",
@@ -125,9 +139,9 @@ TimeEntry.create( name:       "期末レポートチェック",
   t_collect = Task.create( name:        "シャトルシートの回収",
                            description: "シャトルシートの回収業務",
                            deadline:    DateTime.new(2014, 4, 17, 11, 55, 0, '+9') + (WEEK * n-1),
-                           status:      true,
                            keyword:     "シャトルシート",
-                           mission_id:  m_lect.id )
+                           mission_id:  m_lect.id,
+                           state_id:    done.id )
   TimeEntry.create( name:       "シャトルシートの回収",
                     keyword:    "シャトルシート",
                     comment:    "特になし",
@@ -138,9 +152,9 @@ TimeEntry.create( name:       "期末レポートチェック",
   t_clear = Task.create( name:        "講義片付け",
                          description: "講義片付け業務",
                          deadline:    DateTime.new(2014, 4, 17, 11, 55, 0, '+9') + (WEEK * n-1),
-                         status:      true,
                          keyword:     "片付け ホワイトボード",
-                         mission_id:  m_lect.id )
+                         mission_id:  m_lect.id,
+                         state_id:    done.id )
   TimeEntry.create( name:       "講義片付け",
                     keyword:    "ホワイトボード",
                     comment:    "ホワイトボードは消し忘れやすいので注意",
@@ -151,9 +165,9 @@ TimeEntry.create( name:       "期末レポートチェック",
   t_data = Task.create( name:        "シャトルシートデータ化",
                         description: "シャトルシートデータ化業務",
                         deadline:    DateTime.new(2014, 4, 17, 12, 30, 0, '+9') + (WEEK * n-1),
-                        status:      true,
                         keyword:     "シャトルシート データ化 pdf",
-                        mission_id:  m_lect.id )
+                        mission_id:  m_lect.id,
+                        state_id:    done.id )
   TimeEntry.create( name:       "シャトルシートデータ化",
                     keyword:    "シャトルシート pdf",
                     comment:    "スキャナの電気を消し忘れないように",
@@ -164,9 +178,9 @@ TimeEntry.create( name:       "期末レポートチェック",
   t_mail = Task.create( name:        "業務日誌送信",
                         description: "業務日誌送信業務",
                         deadline:    DateTime.new(2014, 4, 17, 17, 0, 0, '+9') + (WEEK * n-1),
-                        status:      true,
                         keyword:     "メール 業務日誌",
-                        mission_id:  m_lect.id )
+                        mission_id:  m_lect.id,
+                        state_id:    done.id )
   TimeEntry.create( name:       "業務日誌送信",
                     keyword:    "メール",
                     comment:    "書いてないことをメールの冒頭に書かないこと",
