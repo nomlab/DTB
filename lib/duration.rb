66da21aa3ecf7521b65da1841696d415318f9d3f
Takuya Okada <pitipitiunsyumikan@gmail.com>
2015-01-23 15:26:05 +0900

    Make duration.to_s accepts a option for format

--- lib/duration.rb
+++ lib/duration.rb
@@ -26,7 +26,20 @@ class Duration
     return (@end_time - @start_time).to_i
   end
 
-  def to_s
+  def to_s(option = nil)
+    if option == :hour
+      h, mod = self.to_seconds.divmod(3600)
+      m, s = mod.divmod(60).map do |num|
+        num.to_s.rjust(2, "0")
+      end
+      return "#{h}:#{m}:#{s}"
+    elsif option == :minute
+      m, s = self.to_seconds.divmod(60)
+      s.to_s.rjust(2, "0")
+      return "#{m}:#{s}"
+    elsif option == :second
+      return "#{self.to_seconds}"
+    end
     return "#{@start_time.to_s} - #{@end_time.to_s}"
   end
 end
