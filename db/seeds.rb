cc3cf461880ed0cc9fe6129971616fd1629917a7
Takuya Okada <pitipitiunsyumikan@gmail.com>
2015-03-13 00:28:26 +0900

    Create several states in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,28 @@
-unless State.find_by(name: "TODO")
-  default = State.find_by(default: true).nil?
-  State.create( name: "TODO",
-                color: "#e65757",
-                position: 1,
-                default: default )
-end
-unless State.find_by(name: "DONE")
-  State.create( name: "DONE",
-                color: "#00ff40",
-                position: 2,
-                default: false )
-end
+State.create( name: "TODO",
+              color: "#e65757",
+              position: 1,
+              default: true )
+State.create( name: "STARTED",
+              color: "#00f5ff",
+              position: 2,
+              default: false )
+State.create( name: "WAIT",
+              color: "#4ad0b7",
+              position: 3,
+              default: false )
+State.create( name: "POSTPONE",
+              color: "#ff00ff",
+              position: 4,
+              default: false )
+State.create( name: "MAYBE",
+              color: "#ffe100",
+              position: 5,
+              default: false )
+State.create( name: "CANCELED",
+              color: "#ffdead",
+              position: 6,
+              default: false )
+State.create( name: "DONE",
+              color: "#9fffa6",
+              position: 100,
+              default: false )
