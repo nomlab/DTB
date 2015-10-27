022347491b47139bdb70844773f3662af8df2128
Takuya Okada <pitipitiunsyumikan@gmail.com>
2015-10-27 16:13:23 +0900

    Separate before action function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :set_root_missions
+  before_filter :set_inbox_tasks
 
   private
   def set_root_missions
     @root_missions = Mission.roots
+  end
+
+  def set_inbox_tasks
     @inbox_tasks = Task.unorganized
   end
 end
