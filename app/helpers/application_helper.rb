0af0b2c21fb00ce19510fe5d3cea187fab8e8560
Takuya Okada <pitipitiunsyumikan@gmail.com>
2015-11-24 21:28:40 +0900

    Move helper method to suitable module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,27 +15,6 @@ module ApplicationHelper
                        :data_seconds => seconds)
   end
 
-  def history_icon(history)
-    case history.extension
-    when "eml"
-      icon = "thunderbird"
-    when "html"
-      icon = "firefox"
-    when "org"
-      icon = "emacs"
-    when "pdf"
-      icon = "preview"
-    when "txt"
-      icon = "emacs"
-    when "xlsx"
-      icon = "excel"
-    else
-      return nil
-    end
-    return image_tag( "#{icon}.png",
-                      {class: "history-icon"} )
-  end
-
   def treeview_mission_node(mission)
     return content_tag :li do
       concat colored_state(mission.state)
--- app/helpers/unified_histories_helper.rb
+++ app/helpers/unified_histories_helper.rb
@@ -1,2 +1,22 @@
 module UnifiedHistoriesHelper
+  def history_icon(history)
+    case history.extension
+    when "eml"
+      icon = "thunderbird"
+    when "html"
+      icon = "firefox"
+    when "org"
+      icon = "emacs"
+    when "pdf"
+      icon = "preview"
+    when "txt"
+      icon = "emacs"
+    when "xlsx"
+      icon = "excel"
+    else
+      return nil
+    end
+    return image_tag( "#{icon}.png",
+                      {class: "history-icon"} )
+  end
 end
