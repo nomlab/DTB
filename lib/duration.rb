70cb76498620a673ac1f53725bd489f2c5292abb
Takuya Okada <pitipitiunsyumikan@gmail.com>
2016-01-15 02:17:35 +0900

    Refactor lib/duration.rb

--- lib/duration.rb
+++ lib/duration.rb
@@ -2,24 +2,26 @@ class Duration
   attr_reader :start_time, :end_time
 
   def initialize(s_time = Time.current, e_time = Time.current)
-    s_time ||= Time.current
-    e_time ||= Time.current
     @start_time = s_time
     @end_time = e_time
   end
 
   def ==(other)
-    return false unless other.class == Duration
     @start_time == other.start_time && @end_time == other.end_time
   end
 
   def in?(other)
-    return false unless other.class == Duration
     other.start_time <= @start_time && @end_time <= other.end_time
   end
 
+  def slice(other)
+    return nil unless overlap?(other)
+    s_time = @start_time < other.start_time ? other.start_time : @start_time
+    e_time = other.end_time < @end_time ? other.start_time : @end_time
+    Duration.new(s_time, e_time)
+  end
+
   def merge(other)
-    @end_time ||= @start_time
     s_time = @start_time < other.start_time ? @start_time : other.start_time
     e_time = @end_time > other.end_time ? @end_time : other.end_time
     Duration.new(s_time, e_time)
@@ -33,38 +35,40 @@ class Duration
     (@start_time.to_date)..(@end_time.to_date)
   end
 
-  def slice(other)
-    if overlap?(other)
-      s_time = @start_time < other.start_time ? other.start_time : @start_time
-      e_time = other.end_time < @end_time ? other.start_time : @end_time
-      return Duration.new(s_time, e_time)
-    else
-      return nil
-    end
-  end
-
   def to_seconds
     (@end_time - @start_time).to_i
   end
 
-  def length
-    to_seconds
+  def to_s(option = :duration)
+    case option
+    when :duration
+      "#{@start_time} - #{@end_time}"
+    when :hour
+      h, m, s = calc_hour_minutes_and_seconds(to_seconds)
+      format('%02d:%02d:%02d', h, m, s)
+    when :minute
+      m, s = calc_minutes_and_seconds(to_seconds)
+      format('%02d:%02d', m, s)
+    when :second
+      "#{to_seconds}"
+    else
+      fail 'Unknown option.'
+    end
   end
 
-  def to_s(option = nil)
-    if option == :hour
-      h, mod = to_seconds.divmod(3600)
-      m, s = mod.divmod(60).map do |num|
-        num.to_s.rjust(2, '0')
-      end
-      return "#{h}:#{m}:#{s}"
-    elsif option == :minute
-      m, s = to_seconds.divmod(60)
-      s.to_s.rjust(2, '0')
-      return "#{m}:#{s}"
-    elsif option == :second
-      return "#{to_seconds}"
-    end
-    "#{@start_time} - #{@end_time}"
+  alias_method :s_time, :start_time
+  alias_method :e_time, :end_time
+  alias_method :length, :to_seconds
+
+  private
+
+  def calc_hour_minutes_and_seconds(sec)
+    hour, mod = sec.divmod(3600)
+    minutes, seconds = calc_minutes_and_seconds(mod)
+    [hour, minutes, seconds]
+  end
+
+  def calc_minutes_and_seconds(sec)
+    sec.divmod(3600)
   end
 end
