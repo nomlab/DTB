246df0a5f9a5d482d107b8af56336b6e9fb6d2e5
Takuya Okada <pitipitiunsyumikan@gmail.com>
2016-01-15 10:45:31 +0900

    Remove unused error object

--- config/initializers/toggl_api.rb
+++ config/initializers/toggl_api.rb
@@ -3,7 +3,7 @@ cli = Toggl::Base.new(ApplicationSettings.toggl.token)
 begin
   cli.me(true)
   TOGGL_API_CLIENT = cli
-rescue => e
+rescue
   warn 'Warn: Your Toggl API is invalid. DTB will record work time by itself.'
   TOGGL_API_CLIENT = nil
 end
