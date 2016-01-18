8e9288ca5f294acca7e7be81d7baa34fde446648
Takuya Okada <pitipitiunsyumikan@gmail.com>
2016-01-18 11:04:08 +0900

    Integrate specific time ranges histories

--- local/all_history_integrater.rb
+++ local/all_history_integrater.rb
@@ -1,3 +1,14 @@
+# RAILS_ENV=production bundle exec rails runner local/all_history_integrater.rb "2015/01/01 10:00:00" "2016/01/01 11:00:00"
+
+# make durations
+durs = []
+ARGV.each_slice(2) do |s_time, e_time|
+  durs << Duration.new(Time.zone.parse(s_time), Time.zone.parse(e_time))
+end
+
+uhs = UnifiedHistory.all
+uhs = uhs.overlap(durs) unless durs.blank?
+
 integrated_histories = IntegratedHistory.integrate(UnifiedHistory.all)
 
 puts "タイトル,パス,重要度,成果物あるいは参考資料か"
--- local/history_integrater.rb
+++ local/history_integrater.rb
@@ -1,3 +1,4 @@
+# RAILS_ENV=production bundle exec rails runner local/history_integrater.rb Emacs "2015/01/01 10:00:00" "2016/01/01 11:00:00"
 # ARGV[0] : File extension (org, xlsx, pptx...)
 
 if ARGV[0].nil?
@@ -15,18 +16,27 @@ def app_to_ext(app_name)
   table[app_name]
 end
 
-unless ext = app_to_ext(ARGV[0])
+unless ext = app_to_ext(ARGV.shift)
   puts "Error: Unrecognized APP_NAME."
   exit
+end
+
+# make durations
+durs = []
+ARGV.each_slice(2) do |s_time, e_time|
+  durs << Duration.new(Time.zone.parse(s_time), Time.zone.parse(e_time))
 end
 
 case ext
 when :other
   title = UnifiedHistory.arel_table[:title]
   uhs = UnifiedHistory.where.not(title.matches("%.pdf")).where.not(title.matches("%.xlsx")).where.not(title.matches("%.pptx")).file_histories
+  uhs = uhs.overlap(durs) unless durs.blank?
   integrated_histories = uhs.group_by{|uh| uh.path}.map{|path, uhs| IntegratedHistory.new(uhs)}
 else
-  integrated_histories = UnifiedHistory.extension(ext).group_by{|uh| uh.path}.map{|path, uhs| IntegratedHistory.new(uhs)}
+  uhs = UnifiedHistory.extension(ext)
+  uhs = uhs.overlap(durs) unless durs.blank?
+  integrated_histories = uhs.group_by{|uh| uh.path}.map{|path, uhs| IntegratedHistory.new(uhs)}
 end
 
 integrated_histories.each do |h|
